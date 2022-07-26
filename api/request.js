4609014c21eff578d86811a8e6385b05a9ba6f9d
sbustilloc <sbustillo@gmail.com>
2022-07-26 13:41:02 -0500

    Act. codigo

--- api/request.js
+++ api/request.js
@@ -1,32 +1,77 @@
 const ApiURL = "https://my-json-server.typicode.com/sbustilloc/M3U2TrelloApp-sbustilloc";
 
+//Hacemos una petición get a la API Fake utlizando los templates strings
 axios
-    .get(`${ApiURL}/task`)
-    .then((res) => showAllTask(res.data))
-    .catch((err) => console.error(err))
+  .get(`${ApiURL}/tasks`)
+  .then((res) => showAllTasks(res.data))
+  .catch((err) => console.error(err));
 
-const showAllTask = (data) => {
-    data.map((task) => createTask(task));
+// Filtramos toda la información que recibimos de la API y la mapeamos
+const showAllTasks = (data) => {
+  data.map((task) => createTask(task));
 };
 
 const createTask = (task) => {
+  // Creamos la estructura de las tarjetas desde el JS
+  let newTask = document.createElement("article");
+  //Creamos un elemento article y le pasamos una clase card-task
+  newTask.classList.add("card");
 
-    let newTask = document.createElement("article");
-    newTask.classList.add("card");
+  // Creamos un H3 para el titulo de las tarjetas
+  let taskTitle = document.createElement("h3");
+  // le añadimos una clase card-task__title
+  taskTitle.classList.add("card-title");
+  // y le añadimos el title de nuestra API "task.title"
+  taskTitle.innerText = task.title;
 
-    let taskTitle = document.createElement("h3");
-    taskTitle.classList.add("card-title");
-    taskTitle.innerText = task.title;
+  //creamos una etiqueta parrafo
+  let taskResponsible = document.createElement("p");
+  //le añadimos una clase card-task__responsible
+  taskResponsible.classList.add("text-danger");
+  // le pasamos los datos del responsable de la tarea desde la API
+  taskResponsible.innerHTML = `<span class="card_task__responsible--tag-creator">Responsable:</span> ${task.person}`;
 
-    let taskResponsible = document.createElement("p");
-    taskResponsible.classList.add("card-responsible");
-    taskResponsible.innerHTML = '<span class="card-responsible">Responsable:</span> ${task.person}';
+  // Creamos una etiqueta parrafo
+  let taskDetails = document.createElement("p");
+  //le añadimos una clase card-task__details
+  taskDetails.classList.add("text");
+  // Le pasamos los datos desde la API y los imprimimos en las tarjetas
+  taskDetails.innerHTML = `<span class="card-task__details--task-details">Descripción:</span> ${task.details} `;
 
-    let taskDetails = document.createElement("p");
-    taskDetails.classList.add("card-details");
-    taskDetails.innerHTML = '<span class="card-details">Descripción: </span>  ${task.details}';
+  //Creamos una etiqueta parrafo para crear la fecha
+  let taskDate = document.createElement("p");
+  //le pasamos la clase card-task__date
+  taskDate.classList.add("text-info");
+  // Le añadimos la fecha que traemos desde la API utilizando los template string
+  taskDate.innerHTML = `<span class="card-task__date--tag-date">Plazo:</span> ${dateFormat(
+    task.deadline
+  )}`;
 
-    let taskDate = document.createElement("p");
-    taskDate.classList.add("card-date");
-    taskDate.innerHTML = '<span class="card-date">Plazo: </span>  ${dateFormat(task.deadline)}';
-}
+  let taskCreate = document.createElement("p");
+  taskCreate.classList.add("text-info");
+  taskCreate.innerHTML = `<span class="card-task__date--tag-date">Creación:</span> ${dateFormat(
+    task.created
+  )}`;
+
+  newTask.appendChild(taskTitle);
+  newTask.appendChild(taskResponsible);
+  newTask.appendChild(taskDetails);
+  newTask.appendChild(taskDate);
+  newTask.appendChild(taskCreate);
+
+  // Referenciamos por medio del ID las columnas
+  let columnToDo = document.querySelector("#todoTasks");
+  let columnInProgress = document.querySelector("#progressTasks");
+  let columnDone = document.querySelector("#doneTasks");
+
+  // Preguntamos dependiendo el state que trae nuestra API es igual a to-do lo ubique en esa columna
+  if (task.state === "pending") {
+    columnToDo.appendChild(newTask);
+  }
+  if (task.state === "progress") {
+    columnInProgress.appendChild(newTask);
+  }
+  if (task.state === "done") {
+    columnDone.appendChild(newTask);
+  }
+};
--- asset/js/index.js
+++ asset/js/index.js
@@ -1,5 +1,5 @@
 ///// fechas
 
-const dateFormat = (time,format = 'DD/MM/YYYY') => {
+/*const dateFormat = (time,format = 'DD/MM/YYYY') => {
     return SVGAnimateMotionElement.unix(time).format(format)
-}
+}*/
